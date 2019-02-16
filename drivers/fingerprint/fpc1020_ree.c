daac0230fffa17db3a1c648c5417951a93356ade
ajaivasudeve <ajaivasudeve@gmail.com>
2019-02-16 10:56:12 +0530

    fpc1020: trigger cpu_input_boost_max on wakeup
    * Boost cpus to max for 1000ms.
    
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -28,6 +28,7 @@
 #include <linux/io.h>
 #include <linux/of_gpio.h>
 #include <linux/input.h>
+#include <linux/cpu_input_boost.h>
 
 #define FPC1020_TOUCH_DEV_NAME  "fpc1020tp"
 
@@ -290,6 +291,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *_fpc1020)
 	 ** since this is interrupt context (other thread...) */
 	smp_rmb();
 	if (fpc1020->wakeup_enabled && !fpc1020->screen_on) {
+		cpu_input_boost_kick_max(1000);
 		pm_wakeup_event(fpc1020->dev, 5000);
 	}
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
